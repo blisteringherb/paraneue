37f749793f452ffe0287111ea4c2bddacee4e8de
Desmond Morris <hi@desmondmorris.com>
2013-09-30 10:37:07 -0400

    Some whitespace

--- paraneue/templates/system/page.tpl.php
+++ paraneue/templates/system/page.tpl.php
@@ -26,15 +26,11 @@
 
 <div role="main" class="wrapper">
   <div class="content-center">
-
     <?php print $messages; ?>
-
     <?php if ($breadcrumb): print $breadcrumb; endif; ?>
-
     <?php print render($title_prefix); ?>
     <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
     <?php print render($title_suffix); ?>
-
     <?php print render($page['content']); ?>
   </div>
 </div>

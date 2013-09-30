4ab2ce4b1806551d180b019b3f48c4b81a10765b
Desmond Morris <hi@desmondmorris.com>
2013-09-30 10:37:30 -0400

    Adds a default compass config

---
+++ paraneue/config.rb
@@ -0,0 +1,12 @@
+# Set this to the root of your project when deployed:
+http_path = "/sites/all/themes/paraneue"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true

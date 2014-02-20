d82bf9de479fb0b1fecf770e8cacbe61e092762b
David Furnes <dfurnes@dosomething.org>
2014-02-20 10:25:16 -0500

    Separated Jobs and Internships links.

--- paraneue/templates/system/page.tpl.php
+++ paraneue/templates/system/page.tpl.php
@@ -82,7 +82,8 @@
         <li><a href="#">What is Do Something?</a></li>
         <li><a href="#">Our Team</a></li>
         <li><a href="#">Scholarships</a></li>
-        <li><a href="#">Jobs &amp; Internships</a></li>
+        <li><a href="#">Jobs</a></li>
+        <li><a href="#">Internships</a></li>
         <li><a href="#">Old People</a></li>
         <li><a href="#">Sexy Financials</a></li>
         <li><a href="#">International</a></li>

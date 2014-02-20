9562696ce5ee056e9f64b63001ea22b8207773b9
David Furnes <dfurnes@dosomething.org>
2014-02-20 10:43:19 -0500

    Adds favicon, touch icon, viewport meta tags.

--- paraneue/templates/system/html.tpl.php
+++ paraneue/templates/system/html.tpl.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html class="no-js">
 <head>
-    <meta charset="utf-8">
-    <?php print $head; ?>
-    <title><?php print $head_title; ?></title>
-    <?php print $styles; ?>
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+  <link rel="shortcut icon" href="/<?php print NEUE_PATH; ?>/assets/images/favicon.ico">
+  <link rel="apple-touch-icon-precomposed" href="/<?php print NEUE_PATH; ?>/assets/images/apple-touch-icon-precomposed.png">
+
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
 </head>
 
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
